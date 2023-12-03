3974cfabba138e0f4d1f7d77b8bac56f71a15f85
Farrel-Saputro <farrel.ardiyanto@ti.ukdw.ac.id>
2023-12-03 19:23:37 +0700

    Login Activity

---
+++ app/src/main/java/com/dicoding/cooknow/ui/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.dicoding.cooknow.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dicoding.cooknow.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
