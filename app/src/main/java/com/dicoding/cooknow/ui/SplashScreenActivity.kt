af1f0749045e605cfab1cec0fe7d097cfe52d432
Farrel-Saputro <farrel.ardiyanto@ti.ukdw.ac.id>
2023-12-06 22:11:11 +0700

    Splash Screen Activity

---
+++ app/src/main/java/com/dicoding/cooknow/ui/SplashScreenActivity.kt
@@ -0,0 +1,40 @@
+package com.dicoding.cooknow.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import com.dicoding.cooknow.R
+import com.dicoding.cooknow.databinding.ActivitySplashScreenBinding
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivitySplashScreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Handler().postDelayed({
+            val user = Firebase.auth.currentUser
+            val intent: Intent
+
+            if (user != null) {
+                // User is logged in
+                Log.d("MainActivity", "User is logged in with email: ${user.email}")
+                intent = Intent(this, MainActivity::class.java)
+            } else {
+                // User is not logged in
+                Log.d("WelcomeActivity", "User is not logged in")
+                intent = Intent(this, WelcomeActivity::class.java)
+            }
+
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
