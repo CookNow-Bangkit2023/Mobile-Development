bb815ab518dde0830ef62d9138ba8ca6f08604eb
Farrel-Saputro <farrel.ardiyanto@ti.ukdw.ac.id>
2023-12-19 16:45:11 +0700

    MotionLayout for SplashScreen

--- app/src/main/java/com/dicoding/cooknow/ui/splashscreen/SplashScreenActivity.kt
+++ app/src/main/java/com/dicoding/cooknow/ui/splashscreen/SplashScreenActivity.kt
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.util.Log
+import android.view.WindowManager
+import androidx.constraintlayout.motion.widget.MotionLayout
+import com.dicoding.cooknow.R
 import com.dicoding.cooknow.databinding.ActivitySplashScreenBinding
 import com.dicoding.cooknow.ui.main.MainActivity
 import com.dicoding.cooknow.ui.welcome.WelcomeActivity
@@ -14,28 +17,68 @@ import com.google.firebase.ktx.Firebase
 class SplashScreenActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivitySplashScreenBinding
+    private lateinit var motionlayout: MotionLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        Handler().postDelayed({
-            val user = Firebase.auth.currentUser
-            val intent: Intent
+        supportActionBar?.hide()
 
-            if (user != null) {
-                // User is logged in
-                Log.d("MainActivity", "User is logged in with email: ${user.email}")
-                intent = Intent(this, MainActivity::class.java)
-            } else {
-                // User is not logged in
-                Log.d("WelcomeActivity", "User is not logged in")
-                intent = Intent(this, WelcomeActivity::class.java)
+        this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        motionlayout = findViewById(R.id.mainlayout)
+        motionlayout.startLayoutAnimation()
+
+        motionlayout.setTransitionListener(object :MotionLayout.TransitionListener{
+            override fun onTransitionStarted(
+                motionLayout: MotionLayout?,
+                startId: Int,
+                endId: Int
+            ) {
+
             }
 
-            startActivity(intent)
-            finish()
-        }, 3000)
+            override fun onTransitionChange(
+                motionLayout: MotionLayout?,
+                startId: Int,
+                endId: Int,
+                progress: Float
+            ) {
+
+            }
+
+            override fun onTransitionCompleted(motionLayout: MotionLayout?, currentId: Int) {
+                Handler().postDelayed({
+                    val user = Firebase.auth.currentUser
+                    val intent: Intent
+
+                    if (user != null) {
+                        // User is logged in
+                        Log.d("MainActivity", "User is logged in with email: ${user.email}")
+                        intent = Intent(this@SplashScreenActivity, MainActivity::class.java)
+                    } else {
+                        // User is not logged in
+                        Log.d("WelcomeActivity", "User is not logged in")
+                        intent = Intent(this@SplashScreenActivity, WelcomeActivity::class.java)
+                    }
+
+                    startActivity(intent)
+                    finish()
+                }, 1000)
+            }
+
+            override fun onTransitionTrigger(
+                motionLayout: MotionLayout?,
+                triggerId: Int,
+                positive: Boolean,
+                progress: Float
+            ) {
+
+            }
+
+        })
+
+
     }
 }
